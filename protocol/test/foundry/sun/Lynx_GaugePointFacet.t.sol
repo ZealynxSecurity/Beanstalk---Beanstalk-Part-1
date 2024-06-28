7908d75d05ff56c9121ac672d73cbc476584256c
Sergio <94926493+scab24@users.noreply.github.com>
2024-06-28 11:30:46 +0200

    Update Lynx_GaugePointFacet.t.sol

--- protocol/test/foundry/sun/Lynx_GaugePointFacet.t.sol
+++ protocol/test/foundry/sun/Lynx_GaugePointFacet.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.6 <0.9.0;
 pragma abicoder v2;
 
 import "forge-std/Test.sol";
-import "../../contracts/beanstalk/sun/GaugePointFacet.sol";
+import "../../../contracts/beanstalk/sun/GaugePointFacet.sol";
 
 contract GaugePointFacetTest is Test {
     GaugePointFacet gaugePointFacet;
